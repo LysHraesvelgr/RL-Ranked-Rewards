2cf1a10ca1ec4031ef792fbd4c8fee2cf577e93e
LysHraesvelgr <elysiaacerola@gmail.com>
2025-05-01 23:10:36 -0700

    Refactor Vite configuration to include Tailwind CSS and Autoprefixer in the CSS setup

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/RL-Ranked-Rewards",
   plugins: [
-    react(),
-    tailwindcss()
+    react()
   ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        require('autoprefixer')
+      ]
+    }
+  }
 })
